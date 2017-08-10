8304da6e05748dcc564c8736b173accc7308f0b6
dwqs <461147874@qq.com>
2017-08-11 01:11:52 +0800

    start 组件

---
+++ gh/components/start/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="start">
+        <h3>快速开始</h3>
+        <div class="install">
+            <h4>安装</h4>
+            <pre>npm i --save vue-area-linkage</pre>
+            <p>或者</p>
+            <pre>yarn add vue-area-linkage</pre>
+            <p>或者</p>
+            <pre>bower install vue-area-linkage</pre>
+        </div>
+        <div class="register">
+            <h4>全局注册</h4>
+            <pre>import Vue from 'vue';<br/><br/>import VueAreaLinkage from 'vue-area-linkage';<br/><br/>Vue.use(VueAreaLinkage);<br/></pre>
+        </div>
+        <br/>
+    </div>
+</template>
+
+<script>
+    import './index.less';
+    export default {
+
+    }
+</script>
