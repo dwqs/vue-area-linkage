f043e5cc1688db99c8ef12c48980517f1cbf6827
dwqs <461147874@qq.com>
2017-08-10 18:41:32 +0800

    main 组件

---
+++ gh/components/main/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="app-main">
+         <area-select></area-select>
+    </div>
+</template>
+
+<script>
+    import './index.less';
+    
+    export default {
+        data() {
+            return {
+
+            }
+        },
+    };
+
+</script>
