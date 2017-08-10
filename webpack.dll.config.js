2df6046542e12088e436530b41f1c0ae14a94725
dwqs <461147874@qq.com>
2017-08-10 13:41:47 +0800

    更改输出目录

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -8,14 +8,14 @@ module.exports = {
         vendor: ['vue', 'element-ui', 'china-area-data']
     },
     output: {
-        path: path.join(__dirname, './dist'),
+        path: path.join(__dirname, './demo'),
         filename: '[name].dll.js',
         //定义输出：window.${output.library}
         library: '[name]_library'
     },
     plugins: [
         new webpack.DllPlugin({
-          path: path.join(__dirname, './dist', '[name]-manifest.json'),
+          path: path.join(__dirname, './demo', '[name]-manifest.json'),
           // 和 output.library 一样即可
           name: '[name]_library'
       })
