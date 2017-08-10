4f040de1c688b2951506b25b4cbbd5cfe2465f0c
dwqs <461147874@qq.com>
2017-08-10 18:42:34 +0800

    cascader 组件

---
+++ src/area-cascader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="area-cascader">
+        area-cascader
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'area-cascader',
+        props: {
+
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
