ed5cd3d6dd95130a6d8f7127048ccf3fb440f99b
dwqs <461147874@qq.com>
2017-08-10 18:40:53 +0800

    demo build

---
+++ gh/components/footer/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+        <p>最新数据来源：<a href="https://github.com/dwqs/area-data" target="_blank">省/市/区/街道数据</a></p>
+        <p>官方数据：<a href="http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/index.html" target="_blank">国家统计局-城乡划分</a></p>
+        <p>官方数据：<a href="http://www.stats.gov.cn/tjsj/tjbz/xzqhdm/201703/t20170310_1471429.html" target="_blank">国家统计局-县及以上行政区划分</a></p>
+    </footer>
+</template>
+
+<style>
+    footer{
+        margin-top: 30px;
+        text-align: center;
+        color: #999
+    }
+    footer p{
+        margin-top: 15px;
+        cursor: pointer;
+    }
+    footer p:hover{
+        color: #008efd
+    }
+</style>
