04d6adcf57deba62390c422be25e3275287dd6a8
dwqs <461147874@qq.com>
2017-08-11 15:38:58 +0800

    支持选择返回值

--- src/area-cascader.vue
+++ src/area-cascader.vue
@@ -2,7 +2,6 @@
     <div class="area-cascader">
         <el-cascader 
             :options="options" 
-            v-model="selectedOptions" 
             @change="handleChange">
         </el-cascader>
     </div>
@@ -11,27 +10,82 @@
 <script>
     import AreaData from 'area-data';
 
+    import { assert } from './utils';
+
     export default {
         name: 'area-cascader',
         props: {
+            value: {
+                required: true
+            },
+            type: {
+                type: String,
+                default: 'code',  //  code-返回行政区域代码 text-返回文本 all-返回 code 和 text
+                validator: (val) => ['all', 'code', 'text'].indexOf(val) > -1
+            },
             level: {
                 type: Number,
-                default: 1, // 0-->一联 1->二联 2->三联 
-                validator: (val) => [0, 1, 2].indexOf(val) > -1
+                default: 0, // 0->二联 1->三联 
+                validator: (val) => [0, 1].indexOf(val) > -1
             }
         },
 
         data () {
             return {
                 // only array
-                selectedOptions: [],
                 options: []
             };
         },
 
         methods: {
-            handleChange (val) {
-                console.log('handlechange', val);
+            getAreaText(selected) {
+                let texts = [];
+                let provinces = AreaData['86'];
+
+                for(let i = 0, l = selected.length; i < l; i++) {
+                    switch(i) {
+                        case 0:
+                            texts.push(provinces[selected[i]]);
+                            break;
+                        case 1: 
+                            let city = AreaData[selected[0]][selected[i]];
+                            texts.push(city);
+                            break;
+                        case 2: 
+                            let area = AreaData[selected[1]][selected[i]]; 
+                            texts.push(area);
+                            break;          
+                    }
+                }
+
+                return texts;
+            },
+
+            getAll(selected) {
+                let all = [];
+                let texts = this.getAreaText(selected);
+
+                assert(texts.length === selected.length, '获取数据出错了');
+
+                for(let i = 0, l = texts.length; i < l; i++) {
+                    let item = {
+                        [selected[i]]: texts[i]
+                    };
+                    all.push(item);
+                }
+
+                return all;
+            },
+
+            handleChange (selected) {
+                console.log('handlechange', selected);
+                if(this.type === 'code') {
+                    this.$emit('input', selected);
+                } else if(this.type === 'text') {
+                    this.$emit('input', this.getAreaText(selected));
+                } else {
+                    this.$emit('input', this.getAll(selected));
+                }
             },
 
             iterate (obj) {
@@ -80,17 +134,17 @@
         },
 
         created () {
-            if (this.level === 0) {
-                this.options = this.iterate(AreaData['86']);
-                return;
-            }
+            // if (this.level === 0) {
+            //     this.options = this.iterate(AreaData['86']);
+            //     return;
+            // }
 
-            if (this.level === 1) {
+            if (this.level === 0) {
                 this.options = this.iterateCities();
                 return;
             }
 
-            if (this.level === 2) {
+            if (this.level === 1) {
                 this.options = this.iterateAreas();
                 return;
             }
