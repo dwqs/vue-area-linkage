6a686d60ec7bcf0cf24db8779594e77e51b2d5bf
dwqs <461147874@qq.com>
2017-08-11 11:42:29 +0800

    返回区域代码和文本 demo

---
+++ gh/components/main/area-code.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="code-area">
+        <div class="area-left">
+            <area-select type='all' v-model="selected"></area-select>
+        </div>
+        <div class="area-right">
+            <pre><code>{{selected}}</code></pre>
+        </div>
+        <div class="original-code" ref="original" v-if="shown">
+            <pre><code><span>//返回值是一个数组，数组项是包含省市的行政区域代码和文本的对象</span><br /><span>&lt;</span>template<span>&gt;</span><br/>&nbsp;&nbsp;&nbsp;&nbsp;<span>&lt;</span>area-select&nbsp;type='all'&nbsp;v-model='selected'&gt;&lt;/area-select&gt;<br/>&lt;</span>/template<span>&gt;</span></code></pre>
+        </div>
+        <div class="show-code" @click="toggle">
+            {{this.shown ? 'Hide Code' : 'Show Code'}}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                selected: [],
+                shown: false
+            }
+        },
+        methods: {
+            toggle() {
+                this.shown = !this.shown;
+            }
+        }
+    };
+
+</script>
