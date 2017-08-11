6556cdc6d0d2bc9f0f531819f9eb6bf80b916df2
dwqs <461147874@qq.com>
2017-08-11 19:01:13 +0800

    build demo

--- gh/components/main/cas-area-code.vue
+++ gh/components/main/cas-area-code.vue
@@ -7,7 +7,7 @@
             <pre><code>{{selected}}</code></pre>
         </div>
         <div class="original-code" ref="original" v-if="shown">
-            <pre><code><span>//返回值是一个数组，分别是省市的行政区域文本</span><br /><span>&lt;</span>template<span>&gt;</span><br/>&nbsp;&nbsp;&nbsp;&nbsp;<span>&lt;</span>area-cascader&nbsp;type='all'&nbsp;v-model='selected'&gt;&lt;/area-cascader&gt;<br/>&lt;</span>/template<span>&gt;</span></code></pre>
+            <pre><code><span>//返回值是一个数组，数组项是包含行政区域代码和文本的对象</span><br /><span>&lt;</span>template<span>&gt;</span><br/>&nbsp;&nbsp;&nbsp;&nbsp;<span>&lt;</span>area-cascader&nbsp;type='all'&nbsp;v-model='selected'&gt;&lt;/area-cascader&gt;<br/>&lt;</span>/template<span>&gt;</span></code></pre>
         </div>
         <div class="show-code" @click="toggle">
             {{this.shown ? 'Hide Code' : 'Show Code'}}
