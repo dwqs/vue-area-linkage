b0b068febfa31557ca77c261363f2115db338f5b
dwqs <461147874@qq.com>
2017-08-11 18:22:58 +0800

    update

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import {
 import AreaSelect from './area-select.vue';
 import AreaCascader from './area-cascader.vue';
 
-export default function install (Vue) {
+import './icon.css';
+
+function install (Vue) {
     Vue.use(Select);
     Vue.use(Option);
     Vue.use(Cascader);
-
     Vue.component(AreaSelect.name, AreaSelect);
     Vue.component(AreaCascader.name, AreaCascader);
 }
+const VueAreaLinkage = {
+    install
+};
+
+export default VueAreaLinkage;
