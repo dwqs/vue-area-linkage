6dcad0187431a7d38a863eb3d6e6cae1b2c69d0f
dwqs <461147874@qq.com>
2017-08-11 15:08:34 +0800

    设置默认值组件

---
+++ gh/components/main/default-value.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="code-area">
+        <div class="area-left">
+            <area-select :level="2" :default-value="['440000','440300','440305']" v-model="selected"></area-select>
+        </div>
+        <div class="area-right">
+            <pre><code>{{selected}}</code></pre>
+        </div>
+        <div class="original-code" ref="original" v-if="shown">
+            <pre><code><span>//设置默认值，代码对应的区域文本是['广东省', '深圳市', '南山区']</span><br /><span>&lt;</span>template<span>&gt;</span><br/>&nbsp;&nbsp;&nbsp;&nbsp;<span>&lt;</span>area-select&nbsp;:label="2"&nbsp;:default-value=['440000','440300','440305']&nbsp;v-model='selected'&gt;&lt;/area-select&gt;<br/>&lt;</span>/template<span>&gt;</span></code></pre>
+        </div>
+        <div class="show-code" @click="toggle">
+            {{this.shown ? 'Hide Code' : 'Show Code'}}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                selected: [],
+                shown: false
+            }
+        },
+        methods: {
+            toggle() {
+                this.shown = !this.shown;
+            }
+        }
+    };
+
+</script>
