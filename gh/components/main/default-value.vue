2ba8b43779f10bf790c7a559bcb7ca0d5ea8e2f9
dwqs <461147874@qq.com>
2017-08-13 17:09:07 +0800

    update

--- gh/components/main/default-value.vue
+++ gh/components/main/default-value.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="code-area">
         <div class="area-left">
-            <area-select :level="2" :default-value="['440000','440300','440305']" v-model="selected"></area-select>
+            <area-select :level="2" v-model="selected"></area-select>
         </div>
         <div class="area-right">
             <pre><code>{{selected}}</code></pre>
         </div>
         <div class="original-code" ref="original" v-if="shown">
-            <pre><code><span>//设置默认值，代码对应的区域文本是['广东省', '深圳市', '南山区']</span><br /><span>&lt;</span>template<span>&gt;</span><br/>&nbsp;&nbsp;&nbsp;&nbsp;<span>&lt;</span>area-select&nbsp;:label="2"&nbsp;:default-value=['440000','440300','440305']&nbsp;v-model='selected'&gt;&lt;/area-select&gt;<br/>&lt;</span>/template<span>&gt;</span></code></pre>
+            <pre><code><span>//绑定默认值selected=['440000','440300','440305']，代码对应的区域文本是['广东省', '深圳市', '南山区']</span><br /><span>&lt;</span>template<span>&gt;</span><br/>&nbsp;&nbsp;&nbsp;&nbsp;<span>&lt;</span>area-select&nbsp;:label="2"&nbsp;v-model='selected'&gt;&lt;/area-select&gt;<br/>&lt;</span>/template<span>&gt;</span></code></pre>
         </div>
         <div class="show-code" @click="toggle">
             {{this.shown ? 'Hide Code' : 'Show Code'}}
@@ -19,7 +19,7 @@
     export default {
         data() {
             return {
-                selected: [],
+                selected: ['440000','440300','440305'],
                 shown: false
             }
         },
