6738030129477b59185b047835bf00db5e0465b4
dwqs <461147874@qq.com>
2017-10-21 01:43:21 +0800

    update

--- gh/components/footer/index.vue
+++ gh/components/footer/index.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        <p>React 版本：<a target="_blank" href="https://github.com/dwqs/react-area-linkage">正在开发中...</a></p>
+        <p>React 版本：<a target="_blank" href="https://github.com/dwqs/react-area-linkage">React Area Linkage</a></p>
         <p>最新数据来源：<a href="https://github.com/dwqs/area-data" target="_blank">省/市/区/街道数据</a></p>
         <p>官方数据：<a href="http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/index.html" target="_blank">国家统计局-城乡划分</a></p>
         <p>官方数据：<a href="http://www.stats.gov.cn/tjsj/tjbz/xzqhdm/201703/t20170310_1471429.html" target="_blank">国家统计局-县及以上行政区划分</a></p>
--- gh/components/main/default-value.vue
+++ gh/components/main/default-value.vue
@@ -7,7 +7,7 @@
             <pre><code>{{selected}}</code></pre>
         </div>
         <div class="original-code" ref="original" v-if="shown">
-            <pre><code><span>//绑定默认值selected=['440000','440300','440305']，代码对应的区域文本是['广东省', '深圳市', '南山区']</span><br /><span>&lt;</span>template<span>&gt;</span><br/>&nbsp;&nbsp;&nbsp;&nbsp;<span>&lt;</span>area-select&nbsp;:label="2"&nbsp;v-model='selected'&gt;&lt;/area-select&gt;<br/>&lt;</span>/template<span>&gt;</span></code></pre>
+            <pre><code><span>//绑定默认值selected=['440000','440300','440305']，代码对应的区域文本是['广东省', '深圳市', '南山区']</span><br /><span>&lt;</span>template<span>&gt;</span><br/>&nbsp;&nbsp;&nbsp;&nbsp;<span>&lt;</span>area-select&nbsp;:level="2"&nbsp;v-model='selected'&gt;&lt;/area-select&gt;<br/>&lt;</span>/template<span>&gt;</span></code></pre>
         </div>
         <div class="show-code" @click="toggle">
             {{this.shown ? 'Hide Code' : 'Show Code'}}
