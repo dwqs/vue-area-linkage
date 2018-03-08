f8742c2ad4bea93253239ab1f704ec0c3351774c
dwqs <461147874@qq.com>
2018-03-08 14:26:13 +0800

    更新入口和样式

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import {
-    Select, Option, Cascader
-} from 'element-ui';
+// import {
+//     Select, Option, Cascader
+// } from 'element-ui';
 import 'beautify-scrollbar/dist/index.css';
 
 import AreaSelect from './area-select.vue';
 import AreaCascader from './area-cascader.vue';
 
-import './icon.css';
-
 import './index.less';
 
 function install (Vue) {
-    Vue.use(Select);
-    Vue.use(Option);
-    Vue.use(Cascader);
-    Vue.component(AreaSelect.name, AreaSelect);
-    Vue.component(AreaCascader.name, AreaCascader);
+    // Vue.use(Select);
+    // Vue.use(Option);
+    // Vue.use(Cascader);
+    // Vue.component(AreaSelect.name, AreaSelect);
+    // Vue.component(AreaCascader.name, AreaCascader);
 }
 const VueAreaLinkage = {
     install
