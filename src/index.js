7d57f5200ad02da6bd65106132b2e07a1d9036f2
dwqs <461147874@qq.com>
2018-03-08 20:43:21 +0800

    去掉 element-ui 相关的东西

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-// import {
-//     Select, Option, Cascader
-// } from 'element-ui';
 import 'beautify-scrollbar/dist/index.css';
+import './index.less';
 
 import AreaSelect from './area-select.vue';
 import AreaCascader from './area-cascader.vue';
-
-import './index.less';
+import Bus from './bus.js';
 
 function install (Vue) {
     // Vue.use(Select);
@@ -14,6 +11,7 @@ function install (Vue) {
     // Vue.use(Cascader);
     // Vue.component(AreaSelect.name, AreaSelect);
     // Vue.component(AreaCascader.name, AreaCascader);
+    Bus.saveVueRef(Vue);
 }
 const VueAreaLinkage = {
     install
